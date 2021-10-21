ffd7529379876d8ebd43d32fa306a512089e9252
林轶锽（Lin Yihuang）(linyihuang) <linyihuang@qiyi.com>
2021-10-21 11:10:35 +0800

    Initial Commit

---
+++ testInterview/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testInterview
+//
+//  Created by linxiaoyi on 2021/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
