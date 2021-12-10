65f1558667809215c0352b7da5e44fb9e8e6d13d
林轶锽（Lin Yihuang）(linyihuang) <linyihuang@qiyi.com>
2021-12-10 12:37:28 +0800

    【Feature】IQY last day

--- testInterview/test.cpp
+++ testInterview/test.cpp
@@ -2,44 +2,7 @@
 //  test.cpp
 //  testInterview
 //
-//  Created by linxiaoyi on 2021/10/28.
+//  Created by linxiaoyi on 2021/12/2.
 //
 
-//#include <stdio.h>
-//#include <iostream>
-//#include <vector>
-//#include <string>
-//
-//int main(){
-//    std::string foo = "foo-string";
-//    std::string bar = "bar_string";
-//    std::vector<std::string> myVector;
-//
-//    myVector.push_back(foo);
-//    myVector.push_back(std::move(bar));
-//
-//
-//    std::cout << "myvector contains:";
-//    for(std::string& x : myVector)
-//        std::cout << " " << x;
-//    std::cout << std::endl;
-//
-//    std::cout << "foo: "<< foo << std::endl;
-//    std::cout << "bar: "<< bar << std::endl;
-//}
-
-
-
-struct ListNode {
-    int data;
-    ListNode* next;
-    ListNode():data(0),next(nullptr){};
-};
-
-
-
-
-
-int main(){
-    
-}
+#include <stdio.h>
